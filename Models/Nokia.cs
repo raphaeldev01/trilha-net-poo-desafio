b494fb2109b07a9f8d25a61034364257f5719fbb
Raphael Developer <raphael.developer@hotmail.com>
2025-03-30 21:34:07 -0300

    complete task

--- Models/Nokia.cs
+++ Models/Nokia.cs
@@ -1,8 +1,17 @@
 namespace DesafioPOO.Models
 {
     // TODO: Herdar da classe "Smartphone"
-    public class Nokia
+    public class Nokia : Smartphone
     {
+        public Nokia(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei,memoria) {
+
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine($"Instalando {nomeApp} na WindownStore");
+        }
+
         // TODO: Sobrescrever o método "InstalarAplicativo"
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,3 +1,12 @@
 ﻿using DesafioPOO.Models;
 
+Iphone cel1 = new Iphone("16991345301", "iphone 11", "543151413", 64);
+Nokia cel2 = new Nokia("17991345301", "Nokia tijolo", "543245426", 8);
+
+cel1.Ligar();
+cel2.ReceberLigacao();
+
+cel1.InstalarAplicativo("WhatsApp");
+cel2.InstalarAplicativo("WhatsApp");
+
 // TODO: Realizar os testes com as classes Nokia e Iphone
